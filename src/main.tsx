800ce10bf0479d248fca1c70657368fe3f8890bf
gargandiz <gargandzolla10@gmail.com>
2026-06-15 23:06:14 +0300

    1_SPRINT_2_INIT

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import './src_1/index.css'
-import { App } from './src_1/App.tsx'
+// import { App } from './src_1/App.tsx'
+import { App } from './src_2/App.tsx'
 
 createRoot(document.getElementById('root')!).render(<App />)
---
+++ src/src_2/App.tsx
@@ -0,0 +1,12 @@
+import {useState} from 'react';
+import './App.css';
+import {Modal} from "./components/modal/Modal";
+
+
+export const App = () => {
+    return (
+        <div>
+
+        </div>
+    );
+}
