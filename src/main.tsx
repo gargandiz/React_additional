8661b21fd9128fa66b65f60c23d54079d73f1915
gargandiz <gargandzolla10@gmail.com>
2026-06-19 21:09:30 +0300

    1_SPRINT_3_INIT

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import './src_1/index.css'
 // import { App } from './src_1/App.tsx'
-import { App } from './src_2/App.tsx'
+// import { App } from './src_2/App.tsx'
+import { App } from './src_3/App.tsx'
 
 createRoot(document.getElementById('root')!).render(<App />)
